3068cbdddcc9bb2bd1bbecad2e2dac60aac2fbb5
h963z57 <111640710+h963z57@users.noreply.github.com>
2022-08-30 16:16:20 +0500

    Update entrypoint.sh

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+ln -s /tmp/phpMyAdmin.config.inc.php /var/www/html/config.inc.php
 echo "Start nginx & php-fpm"
 nginx
 service php8.0-fpm start
 
-#‡десь мы вешаем бесконечный просмотр лога nginx который будет показывать ошибки пока контейнер запущен
-tail -f /var/log/nginx/error.log
+
+#вЂЎРґРµСЃСЊ РјС‹ РІРµС€Р°РµРј Р±РµСЃРєРѕРЅРµС‡РЅС‹Р№ РїСЂРѕСЃРјРѕС‚СЂ Р»РѕРіР° nginx РєРѕС‚РѕСЂС‹Р№ Р±СѓРґРµС‚ РїРѕРєР°Р·С‹РІР°С‚СЊ РѕС€РёР±РєРё РїРѕРєР° РєРѕРЅС‚РµР№РЅРµСЂ Р·Р°РїСѓС‰РµРЅ
+#tail -f /var/log/nginx/error.log
