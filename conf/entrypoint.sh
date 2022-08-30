50c6ee091e3b0c3e675a6be6e0441161d47fb2ed
h963z57 <111640710+h963z57@users.noreply.github.com>
2022-08-30 16:30:54 +0500

    Update entrypoint.sh

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ln -s /tmp/phpMyAdmin.config.inc.php /var/www/html/config.inc.php
+#ln -s /tmp/phpMyAdmin.config.inc.php /var/www/html/config.inc.php
 echo "Start nginx & php-fpm"
 nginx
 service php8.0-fpm start
