ebdd6d0e4807d5a5449ad7dee25a41c12e29d3d8
h963z57 <111640710+h963z57@users.noreply.github.com>
2022-08-30 17:16:22 +0500

    Update entrypoint.sh

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -2,3 +2,6 @@
 echo "Start nginx & php-fpm"
 nginx
 service php8.0-fpm start
+
+#‡десь мы вешаем бесконечный просмотр лога nginx который будет показывать ошибки пока контейнер запущен
+tail -f /var/log/nginx/error.log
